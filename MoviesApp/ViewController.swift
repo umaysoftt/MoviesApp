a0006657abf320a3db438bf90990f99a1f25f81d
ISMAIL PALALI <ismail.palali.pp@gmail.com>
2022-12-03 01:53:03 +0300

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by İsmail Palalı on 3.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
